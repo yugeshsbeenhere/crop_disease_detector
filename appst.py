95610d1aca8d3ac495452c895c2bbff1144b2454
yugesh verma <60872531+yugeshsbeenhere@users.noreply.github.com>
2023-08-12 13:32:55 +0530

    Add files via upload

---
+++ appst.py
@@ -0,0 +1,49 @@
+import streamlit as st
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+import json
+
+# Load the TensorFlow Lite model
+interpreter = tf.lite.Interpreter(model_path='C:/Users/Admin/Desktop/crop_disease_detector/model.tflite')
+interpreter.allocate_tensors()
+
+# Get the input and output details
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
+class_labels = {0: 'Apple___Apple_scab', 1: 'Apple___Black_rot', 2: 'Apple___Cedar_apple_rust', 3: 'Apple___healthy', 4: 'Blueberry___healthy', 5: 'Cherry_(including_sour)___Powdery_mildew', 6: 'Cherry_(including_sour)___healthy', 7: 'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot', 8: 'Corn_(maize)___Common_rust_', 9: 'Corn_(maize)___Northern_Leaf_Blight', 10: 'Corn_(maize)___healthy', 11: 'Grape___Black_rot', 12: 'Grape___Esca_(Black_Measles)', 13: 'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)', 14: 'Grape___healthy', 15: 'Orange___Haunglongbing_(Citrus_greening)', 16: 'Peach___Bacterial_spot', 17: 'Peach___healthy', 18: 'Pepper,_bell___Bacterial_spot', 19: 'Pepper,_bell___healthy', 20: 'Potato___Early_blight', 21: 'Potato___Late_blight', 22: 'Potato___healthy', 23: 'Raspberry___healthy', 24: 'Soybean___healthy', 25: 'Squash___Powdery_mildew', 26: 'Strawberry___Leaf_scorch', 27: 'Strawberry___healthy', 28: 'Tomato___Bacterial_spot', 29: 'Tomato___Early_blight', 30: 'Tomato___Late_blight', 31: 'Tomato___Leaf_Mold', 32: 'Tomato___Septoria_leaf_spot', 33: 'Tomato___Spider_mites Two-spotted_spider_mite', 34: 'Tomato___Target_Spot', 35: 'Tomato___Tomato_Yellow_Leaf_Curl_Virus', 36: 'Tomato___Tomato_mosaic_virus', 37: 'Tomato___healthy'}  # Your class labels
+
+def classify(image):
+    # Preprocess the image
+    image = image.resize((224, 224))
+    image = np.array(image) / 255.0
+    image = np.expand_dims(image, axis=0).astype(np.float32)
+
+    # Set the image as the input tensor
+    interpreter.set_tensor(input_details[0]['index'], image)
+
+    # Run the inference
+    interpreter.invoke()
+
+    # Get the output tensor and convert it to probabilities
+    output = interpreter.get_tensor(output_details[0]['index'])
+    probabilities = tf.nn.softmax(output)[0]
+    predicted_index = np.argmax(probabilities)
+    predicted_class = class_labels[predicted_index]
+    confidence = probabilities[predicted_index] * 100
+
+    return predicted_class, confidence
+
+# Streamlit UI
+st.title('Crop Disease Detector')
+
+uploaded_file = st.file_uploader('Choose an image...', type=['jpg', 'png', 'jpeg'])
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption='Uploaded Image', use_column_width=True)
+    st.write('')
+    st.write('Classifying...')
+    predicted_class, confidence = classify(image)
+    st.write(f'Predicted Class: {predicted_class}')
+    st.write(f'Confidence: {confidence:.2f}%')
